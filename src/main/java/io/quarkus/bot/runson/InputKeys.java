f178239511e2fd1f3e63a735751498d8c10eec0e
Guillaume Smet <guillaume.smet@gmail.com>
2026-02-11 17:25:14 +0100

    Allow for more flexibility to configure the instances

--- src/main/java/io/quarkus/bot/runson/InputKeys.java
+++ src/main/java/io/quarkus/bot/runson/InputKeys.java
@@ -8,6 +8,8 @@ public final class InputKeys {
     public static final String UBUNTU_LATEST = "ubuntu-latest";
     public static final String MAGIC_CACHE = "magic-cache";
     public static final String AMI = "ami";
+    public static final String SMALL_INSTANCE = "small-instance";
+    public static final String LARGE_INSTANCE = "large-instance";
 
     private InputKeys() {
     }
